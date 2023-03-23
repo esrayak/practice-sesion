0770a28b5706ef0b985f0c7a5ca1c346ebd8d4bf
Esra Yakisikli <esrayakisikli66@gmail.com>
2023-03-23 13:17:10 -0400

    hi

---
+++ src/main/java/Practice/practice01.java
@@ -0,0 +1,7 @@
+package Practice;
+
+public class practice01 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
--- src/main/java/Practice01/practiceClass01.java
+++
@@ -1,7 +0,0 @@
-package Practice01;
-
-public class practiceClass01 {
-    public static void main(String[] args) {
-        System.out.println("Hello guys");
-    }
-}
